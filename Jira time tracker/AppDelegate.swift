68b93bd287a5d70d14222d182941eaa829618f35
Denis Kudinov <Denis.Kudinov@acronis.com>
2016-11-16 14:16:28 +0300

    Initial Commit

---
+++ Jira time tracker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Jira time tracker
+//
+//  Created by Denis Kudinov on 16/11/2016.
+//  Copyright © 2016 Denis Kudinov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
